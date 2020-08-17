ab7abdb4de5fe822e6cadce3f76d036788bf92dc
mozesoron <oronm@wix.com>
2020-08-17 15:56:58 +0300

    add cors

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const PORT = process.env.PORT || 5000
 const cors = require('cors')
 
 express()
-    .use(cors({
-        "origin": "*",
-        "methods": "GET",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-    })).use(express.static(path.join(__dirname, 'public')))
+    .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-    .get('/data', (req, res) => res.json({data: {allow: true}}))
+    .get('/data', cors({
+        "origin": "*",
+        "methods": "GET",
+        "optionsSuccessStatus": 204
+    }), (req, res) => res.json({data: {allow: true}}))
     .post('/data', (req, res) => res.json({data: {allow: false}}))
     .get('/', (req, res) => res.render('pages/index'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));

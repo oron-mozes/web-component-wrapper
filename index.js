2f0412bb53c8a51128c445b3b305f4de5c1712da
mozesoron <oronm@wix.com>
2020-08-17 15:50:54 +0300

    corss

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = express();
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+const cors = require('cors')
 
-app.get('/data', (req, res) => res.json({data: {allow: true}}));
-app.post('/data', (req, res) => res.json({data: {allow: false}}));
-app.get('/', (req, res) => res.render('pages/index'));
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+express()
+    .use(cors({
+        "origin": "*",
+        "methods": "GET",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    })).use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+    .get('/data', (req, res) => res.json({data: {allow: true}}))
+    .post('/data', (req, res) => res.json({data: {allow: false}}))
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));

9fa6cf6e094c8ccc3b9990ae88d036a5c0845923
mozesoron <oronm@wix.com>
2020-08-17 15:32:22 +0300

    add api calls

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .set('view engine', 'ejs');
+
+app.get('/data', (req, res) => res.json({data: {allow: true}}));
+app.post('/data', (req, res) => res.json({data: {allow: false}}));
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
